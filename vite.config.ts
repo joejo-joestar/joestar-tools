edd278a11be11af08557ca9d532f9bb2e9f395a7
Joe <joecn2704+github@gmail.com>
2025-10-28 11:28:12 +0400

    possible fix for the deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,7 +18,13 @@ export default defineConfig({
       "@shared": path.resolve(__dirname, "./src/shared"),
     },
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    monacoEditorPlugin({
+      languageWorkers: ["json"],
+    }),
+  ],
   server: {
     host: "0.0.0.0", // bind to all interfaces (IPv4 & IPv6)
     port: 5173,
