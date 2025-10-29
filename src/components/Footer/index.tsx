0991c6d2efe3182fa338f79a360150d827e73443
Joe <joecn2704+github@gmail.com>
2025-10-29 13:47:37 +0400

    updated something

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,16 @@
 import "./index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { socials } from "../../shared/socialsList";
-import { NavLink } from "react-router";
 
 const Footer = () => {
-  const hehe = "\x70\x61\x73\x6f\x77\x72\x64";
+  const hehe =
+    "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x6A\x6F\x65\x73\x74\x61\x72\x2E\x69\x73\x2D\x61\x2E\x64\x65\x76\x2F\x70\x61\x73\x6F\x77\x72\x64";
   return (
     <footer>
       <p>
         &copy; {new Date().getFullYear()} Joseph Cijo
-        <NavLink to={hehe}>.</NavLink> All rights reserved
-        <NavLink to={hehe}>.</NavLink>
+        <a href={hehe}>.</a> All rights reserved
+        <a href={hehe}>.</a>
       </p>
       <div className="social-links">
         {socials.map((social, index) => (
